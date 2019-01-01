540acfb8414f99f09f85364db0792b54c7df2ddd
rkatsuya <k1414st@gmail.com>
2019-01-01 19:31:54 +0900

    mod setup.py

--- setup.py
+++ setup.py
@@ -12,14 +12,14 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='sample',
-    version='0.1.0',
-    description='Sample package for Python-Guide.org',
+    name='keras_extention',
+    version='0.0.1',
+    description='keras extention like original layers, metrics, and losses.',
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
+    author='k1414st',
+    author_email='k1414st@gmail.com',
     license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=['tensorflow', 'keras'],
 )
 
