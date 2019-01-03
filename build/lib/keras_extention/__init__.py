f0269bced5431482d063abdaca990817033aa395
rkatsuya <k1414st@gmail.com>
2019-01-03 17:21:43 +0900

    remove build files
    Signed-off-by: rkatsuya <k1414st@gmail.com>

--- build/lib/keras_extention/__init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import absolute_import
-
-from . import layers
-
-__version__ = '0.0.1'
--- build/lib/keras_extention/layers/__init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import absolute_import
-
-from .partial_convolutional import *
-
-__version__ = '0.0.1'
