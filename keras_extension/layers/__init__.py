7023460461ea871d5973b2ca2a1da018f6abfee1
rkatsuya <k1414st@gmail.com>
2019-02-05 21:27:14 +0900

    modify __init__ file

--- keras_extension/layers/__init__.py
+++ keras_extension/layers/__init__.py
@@ -2,6 +2,6 @@ from __future__ import absolute_import
 
 from .partial_convolutional import *
 from .mac import MACCell, MAC
-from .graph import GraphConv
+from .graph import GraphConv, GraphRNN
 
 __version__ = '0.0.1'
