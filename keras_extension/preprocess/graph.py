2a009ee0d2c969bebc0e009c185fb8c34b650594
rkatsuya <k1414st@gmail.com>
2019-02-13 20:27:35 +0900

    passed test

--- keras_extension/preprocess/graph.py
+++ keras_extension/preprocess/graph.py
@@ -22,6 +22,24 @@ def _batch_dot(x, y):
     return np.einsum('ijk,ikl->ijl', x, y)
 
 
+def _get_I(X):
+    """ get identity matrix or batched matrices of X """
+    if X.ndim == 2:
+       I = np.eye(X.shape[-1])
+    elif X.ndim == 3:
+       I = np.expand_dims(np.eye(X.shape[-1]), axis=0)
+    return I
+
+
+def _get_diag(X):
+    """ get diagonal matrix or batched matrices of X """
+    if X.ndim == 2:
+        D = np.diag(X.sum(axis=-1))
+    elif X.ndim == 3:
+        D = np.array([np.diag(m.sum(axis=1)) for m in X])
+    return D
+
+
 def to_laplacian(mtx,
                  binarize=False,
                  normalize=False,
@@ -54,25 +72,19 @@ def to_laplacian(mtx,
     if binarize:
         mtx = np.where(mtx == 0, 0., 1.)
 
-
     # case of ndim(2 or 3) x normalize(Do or Not).
-    if mtx.ndim == 2:
-        D = np.diag(mtx.sum(axis=-1))
-        if normalize:
-            # normalize adjacency matrix. (A -> D^1/2 `dot` A `dot` D^1/2)
-            mtx = np.sqrt(D).dot(mtx.dot(np.sqrt(D)))
-            I = np.eye(mtx.shape[-1])
-            mtx_laplacian = I - mtx
-        else:
-            mtx_laplacian = D - mtx
-    elif mtx.ndim == 3:
-        D = np.array([np.diag(m.sum(axis=1)) for m in mtx])
-        if normalize:
-            mtx = _batch_dot(np.sqrt(D), _batch_dot(mtx, np.sqrt(D)))
-            I = np.expand_dims(np.eye(mtx.shape[-1]), axis=0)
-            mtx_laplacian = I - mtx
-        else:
-            mtx_laplacian = D - mtx
+    D = _get_diag(mtx)
+    I = _get_I(mtx)
+    if normalize:
+        Dr = np.where(D == 0, 0,
+             np.power(np.where(D > epsilon, D, epsilon), -0.5))
+        if mtx.ndim == 2:
+            mtx = Dr.dot(mtx.dot(Dr))
+        elif mtx.ndim == 3:
+            mtx = _batch_dot(Dr, _batch_dot(mtx, Dr))
+        mtx_laplacian = I - mtx
+    else:
+        mtx_laplacian = D - mtx
 
     # batch & sparse -> np.array of csr_matrix
     if mtx.ndim == 3 and matrix_type == csr_matrix:
@@ -85,6 +97,7 @@ def to_laplacian(mtx,
 def normalize_graph_matrix(mtx,
                            binarize=False,
                            add_self=False,
+                           add_diagonal=False,
                            normalize=False,
                            normalize_input=False,
                            normalize_output=False,
@@ -99,7 +112,10 @@ def normalize_graph_matrix(mtx,
             (2D square matrix or 3D batched square matrix)
         binarize: binarize weighted matrix.
             (if element != 0, binarize to 1)
-        add_self: add Identify matrix (self loop) before normalize.
+        add_self: add identify matrix (self loop) after normalize.
+            You can use this option w/o normalize option.
+        add_diagonal: add diagonal matrix (weighted self loop)
+            after normalize. You can use this option w/o normalize option.
         normalize: normalize self-adjacency matrix.
             (A -> D^1/2 `dot` A `dot` D^1/2)
         normalize_input: normalize graph input
@@ -120,30 +136,50 @@ def normalize_graph_matrix(mtx,
             'matrix type must be "numpy.array" or "scipy.sparse.csr.csr_matrix".')
     if normalize + normalize_input + normalize_output > 1:
         raise ValueError('multiple normalize options cannt be selected.')
+    if normalize and (add_self or add_diagonal):
+        raise ValueError('you can use add_self or add_diagonal option w/o normalize option.')
+    if add_self and add_diagonal:
+        raise ValueError('you cannot set add_self and add_diagonal simultaneously.')
+
 
     # fundamental preprocess
     if binarize:
         mtx = np.where(mtx == 0, 0., 1.)
-    if add_self:
-        mtx += np.eye(mtx.shape[-1])
+    if normalize:
+        mtx = mtx + np.eye(mtx.shape[-1])
 
     # normalize adjacency matrix. (A -> D^1/2 `dot` A `dot` D^1/2)
+    D = _get_diag(mtx)
+    I = _get_I(mtx)
     if normalize:
+        Dr = np.where(D == 0, 0,
+             np.power(np.where(D > epsilon, D, epsilon), -0.5))
         if mtx.ndim == 2:
-            D = np.diag(mtx.sum(axis=-1))
-            mtx = np.sqrt(D).dot(mtx.dot(np.sqrt(D)))
+            mtx = Dr.dot(mtx.dot(Dr))
         elif mtx.ndim == 3:
-            D = np.array([np.diag(m.sum(axis=1)) for m in mtx])
-            mtx = _batch_dot(np.sqrt(D), _batch_dot(mtx, np.sqrt(D)))
+            mtx = _batch_dot(Dr, _batch_dot(mtx, Dr))
 
+    # normalization of input or output.
     elif normalize_input:
-        D = mtx.sum(axis=-1)
-        D = np.where(D > epsilon, D, epsilon)
-        mtx = np.einsum('ijk,ij->ijk', mtx, 1/D)
+        H = mtx.sum(axis=-1)
+        H = np.where(H > epsilon, H, epsilon)
+        if mtx.ndim == 2:
+            mtx = np.einsum('jk,j->jk', mtx, 1/H)
+        elif mtx.ndim == 3:
+            mtx = np.einsum('ijk,ij->ijk', mtx, 1/H)
     elif normalize_output:
-        D = mtx.sum(axis=-2)
-        D = np.where(D > epsilon, D, epsilon)
-        mtx = np.einsum('ijk,ik->ijk', mtx, 1/D)
+        V = mtx.sum(axis=-2)
+        V = np.where(V > epsilon, V, epsilon)
+        if mtx.ndim == 2:
+            mtx = np.einsum('jk,k->jk', mtx, 1/V)
+        elif mtx.ndim == 3:
+            mtx = np.einsum('ijk,ik->ijk', mtx, 1/V)
+
+    # adding self loop elements.
+    if add_self:
+        mtx = mtx + I
+    if add_diagonal:
+        mtx = mtx + _get_diag(mtx)  # recalc after normalize.
 
     # batch & sparse -> np.array of csr_matrix
     if mtx.ndim == 3 and matrix_type == csr_matrix:
@@ -151,3 +187,5 @@ def normalize_graph_matrix(mtx,
     # np.array or single csr_matrix
     else:
         return matrix_type(mtx)
+
+
