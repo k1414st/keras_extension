1fd7e622b272aa8b22b99f1ae747dc42c955393a
k1414st <k1414st@gmail.com>
2020-05-13 02:56:17 +0000

    fix function in lambda

--- tests/test_sparse_lambda.py
+++ tests/test_sparse_lambda.py
@@ -1,14 +1,18 @@
 import numpy as np
 from scipy.sparse.csr import csr_matrix
-from keras.layers import Input
-from keras.models import Model
+from tensorflow.keras.layers import Input
+from tensorflow.keras.models import Model
 from keras_extension.layers import Lambda
 
 import tensorflow as tf
 
+tf.compat.v1.disable_eager_execution()
+
 SPARSITY = 0.01
-N_DATA = 100
-BATCH_SIZE = 2
+# N_DATA = 100
+# BATCH_SIZE = 2
+N_DATA = 20
+BATCH_SIZE = 20
 DIM_SPARSE = 2**16
 
 ##################################################################################
@@ -19,11 +23,12 @@ def get_model(test_list_case=False):
     x = input_layer
 
     def sparse_matmul(x):
-        w = tf.Variable(tf.zeros([DIM_SPARSE, 1]), dtype=tf.float32)
+        # w = tf.Variable(tf.ones([DIM_SPARSE, 1]), dtype=tf.float32)
+        w = tf.ones([DIM_SPARSE, 1], dtype=tf.float32)
         if not test_list_case:
-            return tf.sparse.matmul(x, w)
+            return tf.sparse.sparse_dense_matmul(x, w)
         else:
-            return tf.sparse.matmul(x[0], w)
+            return tf.sparse.sparse_dense_matmul(x[0], w)
 
     if not test_list_case:
         output_layer = Lambda(sparse_matmul)(x)
@@ -45,16 +50,17 @@ y = np.random.binomial(n=1, p=0.5, size=(N_DATA, 1)).astype(np.float32)
 def test_single_case():
     """ test of single_sparse_input_function model """
     model = get_model()
-    model.fit(x, y, batch_size=2)
+    model.fit(x, y, batch_size=BATCH_SIZE, steps_per_epoch=N_DATA)
     return True
 
 def test_list_case():
     """ test of sparse_list_input_function model """
     model = get_model(test_list_case=True)
-    model.fit(x, y, batch_size=2)
+    model.fit(x, y, batch_size=BATCH_SIZE, steps_per_epoch=N_DATA)
     return True
 
 
 if __name__ == '__main__':
     test_single_case()
-    # test_list_case()
+    test_list_case()
+
